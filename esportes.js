6b383e228876365bcb36a8ac5e24d56441ea0f60
yaraki01 <107232087+yaraki01@users.noreply.github.com>
2025-02-09 13:45:59 -0300

    Add files via upload

---
+++ esportes.js
@@ -0,0 +1,55 @@
+const home = document.getElementById('home')
+
+home.addEventListener('click', function(){
+    window.location.href = 'index.html'
+})
+
+const infantil = document.getElementById('infantil')
+const fundamental = document.getElementById('fundamental')
+const medio = document.getElementById('medio')
+
+infantil.addEventListener('click', function(){
+    window.location.href = 'infantil.html'
+})
+fundamental.addEventListener('click', function(){
+    window.location.href = 'fundamental.html'
+})
+medio.addEventListener('click', function(){
+    window.location.href = 'medio.html'
+})
+
+const unidade1 = document.getElementById('bunidade1')
+const unidade2 = document.getElementById('bunidade2')
+
+unidade1.addEventListener('click', function(){
+    window.location.href = 'https://www.google.com/maps/place/SELM+-+Sistema+de+Ensino+L%C3%A1pis+na+M%C3%A3o/@-7.1825704,-34.8656256,381m/data=!3m1!1e3!4m6!3m5!1s0x7ace9b4e60b3e9d:0x36d966101895d28!8m2!3d-7.1827787!4d-34.8646667!16s%2Fg%2F11ghff2r2l?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D'
+})
+unidade2.addEventListener('click', function(){
+    window.location.href = 'https://www.google.com/maps/place/SELM+-+Sistema+de+Ensino+L%C3%A1pis+na+M%C3%A3o+2/@-7.1828155,-34.864086,270m/data=!3m1!1e3!4m6!3m5!1s0x7ace962f5e51ebf:0x4e0269a36f67b10e!8m2!3d-7.1830463!4d-34.8635418!16s%2Fg%2F11shxhc394?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D'
+})
+
+const futsal = document.getElementById('futsal')
+const ballet = document.getElementById('ballet')
+const judo = document.getElementById('judo')
+
+futsal.addEventListener('click', function(){
+    window.location.href = 'futsal.html'
+})
+ballet.addEventListener('click', function(){
+    window.location.href = 'ballet.html'
+})
+judo.addEventListener('click', function(){
+    window.location.href = 'judo.html'
+})
+
+const instagram = document.getElementById('instagram')
+
+instagram.addEventListener('click', function(){
+    window.location.href = 'https://www.instagram.com/vemserselm/'
+})
+const btn13 = document.getElementById('button13')
+
+btn13.addEventListener('click', function(){
+    window.location.href = 'https://www.instagram.com/vemserselm/'
+})
+
---
+++ home.js
@@ -0,0 +1,85 @@
+const home = document.getElementById('home')
+
+home.addEventListener('click', function(){
+    window.location.href = 'index.html'
+})
+
+const infantil = document.getElementById('infantil')
+const fundamental = document.getElementById('fundamental')
+const medio = document.getElementById('medio')
+
+infantil.addEventListener('click', function(){
+    window.location.href = 'infantil.html'
+})
+fundamental.addEventListener('click', function(){
+    window.location.href = 'fundamental.html'
+})
+medio.addEventListener('click', function(){
+    window.location.href = 'medio.html'
+})
+
+const unidade1 = document.getElementById('bunidade1')
+const unidade2 = document.getElementById('bunidade2')
+
+unidade1.addEventListener('click', function(){
+    window.location.href = 'https://www.google.com/maps/place/SELM+-+Sistema+de+Ensino+L%C3%A1pis+na+M%C3%A3o/@-7.1825704,-34.8656256,381m/data=!3m1!1e3!4m6!3m5!1s0x7ace9b4e60b3e9d:0x36d966101895d28!8m2!3d-7.1827787!4d-34.8646667!16s%2Fg%2F11ghff2r2l?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D'
+})
+unidade2.addEventListener('click', function(){
+    window.location.href = 'https://www.google.com/maps/place/SELM+-+Sistema+de+Ensino+L%C3%A1pis+na+M%C3%A3o+2/@-7.1828155,-34.864086,270m/data=!3m1!1e3!4m6!3m5!1s0x7ace962f5e51ebf:0x4e0269a36f67b10e!8m2!3d-7.1830463!4d-34.8635418!16s%2Fg%2F11shxhc394?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D'
+})
+
+const futsal = document.getElementById('futsal')
+const ballet = document.getElementById('ballet')
+const judo = document.getElementById('judo')
+
+futsal.addEventListener('click', function(){
+    window.location.href = 'futsal.html'
+})
+ballet.addEventListener('click', function(){
+    window.location.href = 'ballet.html'
+})
+judo.addEventListener('click', function(){
+    window.location.href = 'judo.html'
+})
+
+const instagram = document.getElementById('instagram')
+
+instagram.addEventListener('click', function(){
+    window.location.href = 'https://www.instagram.com/vemserselm/'
+})
+
+const locate01 = document.getElementById('locate01')
+const locate02 = document.getElementById('locate02')
+
+locate01.addEventListener('click', function(){
+    window.location.href = 'https://www.google.com/maps/place/SELM+-+Sistema+de+Ensino+L%C3%A1pis+na+M%C3%A3o/@-7.1825704,-34.8656256,381m/data=!3m1!1e3!4m6!3m5!1s0x7ace9b4e60b3e9d:0x36d966101895d28!8m2!3d-7.1827787!4d-34.8646667!16s%2Fg%2F11ghff2r2l?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D'
+})
+locate02.addEventListener('click', function(){
+    window.location.href = 'https://www.google.com/maps/place/SELM+-+Sistema+de+Ensino+L%C3%A1pis+na+M%C3%A3o+2/@-7.1828155,-34.864086,270m/data=!3m1!1e3!4m6!3m5!1s0x7ace962f5e51ebf:0x4e0269a36f67b10e!8m2!3d-7.1830463!4d-34.8635418!16s%2Fg%2F11shxhc394?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D'
+})
+
+const btn12 = document.getElementById('button12')
+
+btn12.addEventListener('click', function(){
+    window.location.href = 'saiba-mais.html'
+})
+
+const conquista = document.getElementById('box9')
+
+conquista.addEventListener('click', function(){
+    window.location.href = 'https://www.educacaoconquista.com.br/'
+})
+
+const btn10 = document.getElementById('button10')
+
+btn10.addEventListener('click', function(){
+    window.location.href = 'https://linktr.ee/selmmatriculas?utm_source=linktree_admin_share&fbclid=PAZXh0bgNhZW0CMTEAAaY12OIyWFUdxnSxStTAtFpansxQ6PPilELF_MTB-VSzIavCvjT-wNmg2LE_aem_ndC39P46iGoOjGmE7ILtzw'
+})
+
+const whatsapp = document.getElementById('faleconosco')
+
+whatsapp.addEventListener('click', function(){
+    window.location.href = 'https://api.whatsapp.com/send?l=pt-BR&phone=558335762568'
+})
+
+
